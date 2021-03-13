c43ce8346049b786d100f8907f3d1df263eb7c12
Timothy Moore <mtimothy984@gmail.com>
2021-03-13 12:58:16 -0800

    Add fast poll trigger file

--- main.py
+++ main.py
@@ -17,25 +17,36 @@ def main():
         help='The key within the bucket where the script is located'
     )
     parser.add_argument(
-        '--poll_interval', required=False, default=300, type=int,
-        help='The number of seconds between checking for the file'
+        '--max_slow_poll_interval', required=False, default=600, type=int,
+        help='The number of seconds between checking for the file on S3.'
+    )
+    parser.add_argument(
+        '--fast_poll_file', required=False, default='/home/ec2-user/s3_poll_cd.trigger', type=str,
+        help='The full path to the file which, when created, immediately triggers a poll'
     )
 
     args = parser.parse_args()
-    poll_continuously(args.s3_bucket, args.s3_key, args.poll_interval)
+    poll_continuously(args.s3_bucket, args.s3_key, args.max_slow_poll_interval, args.fast_poll_file)
 
 
-def poll_continuously(s3_bucket: str, s3_key: str, poll_interval: int):
-    actual_poll_interval = min(poll_interval, 5)
+def poll_continuously(s3_bucket: str, s3_key: str, max_slow_poll_interval: int, fast_poll_file: str):
+    max_slow_poll_interval = max(max_slow_poll_interval, 5)
+
+    fast_polls_per_real_poll = 5
     while True:
+        for idx in range(fast_polls_per_real_poll):
+            if os.path.exists(fast_poll_file):
+                print('Fast poll trigger detected!')
+                os.unlink(fast_poll_file)
+                break
+            time.sleep(1)
+
         print('Polling...')
         if poll(s3_bucket, s3_key):
             print('Executed script')
-            actual_poll_interval = min(poll_interval, 5)
+            fast_polls_per_real_poll = 5
         else:
-            actual_poll_interval = min(poll_interval, actual_poll_interval + 5)
-
-        time.sleep(actual_poll_interval)
+            fast_polls_per_real_poll = min(fast_polls_per_real_poll + 1, int(max_slow_poll_interval))
 
 
 def poll(s3_bucket: str, s3_key: str):
